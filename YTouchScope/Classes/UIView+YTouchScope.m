8cbb752ae8bc117c438992d62e89943cc076579d
李遵源 <zunyuan.li@husor.com.cn>
2017-04-24 20:44:47 +0800

    support temp not superView

--- YTouchScope/Classes/UIView+YTouchScope.m
+++ YTouchScope/Classes/UIView+YTouchScope.m
@@ -12,34 +12,44 @@
 
 @implementation UIView (YTouchScope)
 
-static char YTouchScopeViewsKey;
 - (void)y_touchScopeSize:(CGSize)scopeSize
 {
-    if (!self.superview) {
-        NSAssert(self.superview, @"需要将当前view添加到父view上");
-        return;
-    }
+    [self y_handleWillMoveToSuperview];
     
-    __block NSMutableArray<NSDictionary *> *touchScopeViews = objc_getAssociatedObject(self.superview, &YTouchScopeViewsKey);
-    if (touchScopeViews.count > 0) {
-        [touchScopeViews addObject:@{@"view":self,@"scopeSize":[NSValue valueWithCGSize:scopeSize]}];
-        return;
+    if (self.superview) {
+        [self.superview y_addTouchScopeViews:self scopeSize:CGSizeMake(75, 75)];
+        [self.superview y_handleHitTest];
     }
-    touchScopeViews = [NSMutableArray array];
-    [touchScopeViews addObject:@{@"view":self,@"scopeSize":[NSValue valueWithCGSize:scopeSize]}];
-    
-    [self.superview aspect_hookSelector:@selector(hitTest:withEvent:) withOptions:AspectPositionInstead usingBlock:^(id<AspectInfo> aspectInfo, CGPoint point){
+}
+
+- (void)y_handleWillMoveToSuperview
+{
+    [self aspect_hookSelector:@selector(willMoveToSuperview:) withOptions:AspectPositionInstead usingBlock:^(id<AspectInfo> aspectInfo, UIView *superView){
+        UIView *view = [aspectInfo instance];
+        if (view.superview) {
+            [[view.superview y_aspectToken] remove];
+        }
+        
+        [superView y_addTouchScopeViews:view scopeSize:CGSizeMake(75, 75)];
+        [superView y_handleHitTest];
+        
+    } error:NULL];
+}
+
+- (void)y_handleHitTest
+{
+    [self aspect_hookSelector:@selector(hitTest:withEvent:) withOptions:AspectPositionInstead usingBlock:^(id<AspectInfo> aspectInfo, CGPoint point){
         void * r;
         __block NSInvocation *invocation = aspectInfo.originalInvocation;
         [invocation invoke];
         [invocation getReturnValue:&r];
-        
         UIView *oriHitTestResult = (__bridge UIView *)(r);
         
         if (oriHitTestResult != [aspectInfo instance]) {
             return;
         }
         
+        __block NSMutableArray<NSDictionary *> *touchScopeViews = [[aspectInfo instance] y_touchScopeViwes];
         __block BOOL hasViewContain = NO;
         [touchScopeViews enumerateObjectsUsingBlock:^(NSDictionary *dict, NSUInteger idx, BOOL * _Nonnull stop) {
             UIView *view = dict[@"view"];
@@ -56,8 +66,49 @@ static char YTouchScopeViewsKey;
             }
         }];
     } error:NULL];
+}
+
+#pragma set get
+static char YTouchScopeViewsKey;
+- (NSMutableArray<NSDictionary *> *)y_touchScopeViwes
+{
+    __block NSMutableArray<NSDictionary *> *touchScopeViews = objc_getAssociatedObject(self, &YTouchScopeViewsKey);
+    if (touchScopeViews == nil) {
+        touchScopeViews = [NSMutableArray array];
+    }
+    return touchScopeViews;
+}
+
+- (void)y_addTouchScopeViews:(UIView *)view scopeSize:(CGSize)scopeSize;
+{
+    NSMutableArray<NSDictionary *> *touchScopeViews = [self y_touchScopeViwes];
+    [touchScopeViews addObject:@{@"view":view,@"scopeSize":[NSValue valueWithCGSize:scopeSize]}];
     
-    objc_setAssociatedObject(self.superview, &YTouchScopeViewsKey, touchScopeViews, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    objc_setAssociatedObject(self, &YTouchScopeViewsKey, touchScopeViews, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (void)y_removeTouchScopeViews:(UIView *)view;
+{
+    __block UIView *wantRemoveObj;
+    [[self y_touchScopeViwes] enumerateObjectsUsingBlock:^(NSDictionary * _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        if (obj[@"view"] == view) {
+            wantRemoveObj = obj;
+            *stop = YES;
+        }
+    }];
+    [[self y_touchScopeViwes] removeObject:wantRemoveObj];
+}
+
+
+static char YTouchScopeAspectTokenKey;
+- (void)y_setAspectToken:(id<AspectToken>)aspectToekn
+{
+    objc_setAssociatedObject(self, &YTouchScopeAspectTokenKey, aspectToekn, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (id<AspectToken>)y_aspectToken
+{
+    return objc_getAssociatedObject(self, &YTouchScopeAspectTokenKey);
 }
 
 @end
